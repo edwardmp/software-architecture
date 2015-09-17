ab6df93e8558e410cdd3c9613013f5a380bec3a0
Roy de Wildt <roydewildt@gmail.com>
2015-09-17 14:19:37 +0200

    first codebase

---
+++ Project/QC-Robot/libs/inputHandler/src/main/java/input/HelloWorld.java
@@ -0,0 +1,8 @@
+package input;
+
+/**
+ * Created by roy on 17/09/15.
+ */
+public class HelloWorld {
+    public static void Print(){System.out.println("input!");}
+}
---
+++ Project/QC-Robot/src/main/java/main/run.java
@@ -0,0 +1,7 @@
+package main;
+/**
+ * Created by roy on 17/09/15.
+ */
+public class run {
+    public static void main(String[] args){input.HelloWorld.Print();}
+}
